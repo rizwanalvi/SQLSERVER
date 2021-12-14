f1a9cbadd22cfad3146558db1a44ed17981ec12c
rizwanalvi <rizwan@aptechsite.net>
2021-12-14 18:59:33 +0500

    Create SQL_03.sql

---
+++ SQL_03.sql
@@ -0,0 +1,14 @@
+SELECT * 
+FROM employees
+WHERE salary > 22000
+
+
+SELECT * 
+FROM employees
+WHERE salary >= 22000
+
+
+SELECT * 
+FROM employees
+WHERE salary >=4000 AND salary<=10000
+ORDER BY salary DESC
